3b29660df8521bb6e8a75ceef4f55ddf31f89440
EricZH <letItBezhumphries>
2018-12-26 16:25:07 -0800

    finished exercises 1-6

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Component from 'react';
 
-function App() {
-  return (
-    <div> Hello from Component </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: '',
+      last: ''
+    }
+    this.onSubmitNameChange = this.onSubmitNameChange.bind(this);  
+  }
+  onSubmitNameChange(event) {
+    this.setState({
+      [ event.target.name]: event.target.value
+    })
+  }
+  render() {
+    const {first, last} = this.state;
+    return (
+      <div>
+        First: <input name="first" type="text" onChange={this.onSubmitNameChange} />
+        Last: <input name="last" type="text" onChange={this.onSubmitNameChange} />
+        Hello, {first} {last} !
+      </div>
+    );
+  }  
 }
+
 
 export default App;
